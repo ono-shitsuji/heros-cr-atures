3e3abff8c5ab577e032c2e7563b35b3180ba9d66
ono-shitsuji <fbernard.kylian@gmail.com>
2025-09-08 13:57:33 +0200

    upgraded to 4th

---
+++ data/data.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sunset</title>
+</head>
+<body>
+    <h1>test 1,2</h1>
+    <?php
+    $heros = [
+        [
+            'id' => '0',
+            'name' => 'Toudou Touka',
+            'magic_power' => 'art raikiri',
+            'picture' => 'toudou touka.jpg'
+        ],
+        [
+            'id' => '1',
+            'name' => 'Lucy',
+            'magic_power' => 'vecteur telekinésie',
+            'picture' => 'lucy.jpg'
+        ],
+        [
+            'id' => '2',
+            'name' => 'Esdeath',
+            'magic_power' => 'glace',
+            'picture' => 'esdeath.webp'
+        ],
+        [
+            'id' => '3',
+            'name' => 'Himari Noihara',
+            'magic_power' => 'feu obscur/feu follets',
+            'picture' => 'himari.jpg'
+        ],
+        [
+            'id' => '3',
+            'name' => 'Kurumi',
+            'magic_power' => 'contrôle du temps',
+            'picture' => 'kurumi.webp'
+        ],
+    ];
+    print_r ($heros);
+
+    ?>
+    
+</body>
+</html>
