ff303dafe6f633c71467433a33739a575ffd229a
ono-shitsuji <fbernard.kylian@gmail.com>
2025-09-08 14:38:56 +0200

    unlocked

--- data.php
+++ data.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>sunset</title>
-</head>
-<body>
-    <h1>test 1,2</h1>
+
     <?php
     $heros = [
         [
@@ -40,9 +32,5 @@
             'picture' => 'kurumi.webp'
         ],
     ];
-    print_r ($heros);
-
     ?>
     
-</body>
-</html>
--- index.php
+++ index.php
@@ -1,16 +1,43 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>dawn</title>
-</head>
-<body>
-    <?php
-        require_once  'data/data.php';
+<h1>test 1,2 </h1>
+<?php
+ require_once 'partials/head.php';
+        require_once  'data.php';
         foreach ($heros as $value){
-            echo ($heros . "<br>");
+            ;
         }
+    foreach($heros as $hint =>$value)    
+    require_once 'partials/footer.php';  
+        var_dump($heros);
+   $name = $heros[0]['name'];
+   echo "<p> $name </p>";
+
+    $name = $heros[0]['picture'];
+   echo "<p> $name </p>";
+
+      $name = $heros[1]['name'];
+   echo "<p> $name </p>";
+
+    $name = $heros[1]['picture'];
+   echo "<p> $name </p>";
+
+      $name = $heros[2]['name'];
+   echo "<p> $name </p>";
+
+    $name = $heros[2]['picture'];
+   echo "<p> $name </p>";
+
+      $name = $heros[3]['name'];
+   echo "<p> $name </p>";
+
+    $name = $heros[3]['picture'];
+   echo "<p> $name </p>";
+
+      $name = $heros[4]['name'];
+   echo "<p> $name </p>";
+
+    $name = $heros[4]['picture'];
+   echo "<p> $name </p>"
+
     ?>
     
 </body>
